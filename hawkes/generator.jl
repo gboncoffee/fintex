7b020e4d2ff2ecaf62d0c56a7b201e4ffb78bfc7
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2024-09-15 15:36:05 -0300

    Finished the Hawkes generator

--- hawkes/generator.jl
+++ hawkes/generator.jl
@@ -1,35 +1,38 @@
 using HawkesProcesses, Distributions, Plots
 
+const T = 200
+
 #const bg = 0.5
 #const kappa = 0.5
-#const maxT = 100
-
 struct HawkesGenerator
     bg::Number
     kappa::Number
-    maxT::Number
-end
-
-struct HawkesSimulation
-    ts::Vector{Float64}
-    intensity::Vector{Number}
 end
 
 hawkesKernel(x) = pdf.(Distributions.Exponential(1/0.5), x)
 
-function simulate(gen::HawkesGenerator)::HawkesSimulation
-    simevents = HawkesProcesses.simulate(gen.bg, gen.kappa, hawkesKernel, gen.maxT)
-    ts = collect(0:0.1:gen.maxT)
+function simulate(gen::HawkesGenerator)::Vector{Number}
+    simevents = HawkesProcesses.simulate(gen.bg, gen.kappa, hawkesKernel, T)
+    ts = collect(0:0.1:T)
     intensity = HawkesProcesses.intensity(ts, simevents, gen.bg, gen.kappa, hawkesKernel)
-    return HawkesSimulation(ts, intensity)
+    return intensity
 end
 
-# HawkesSimulation.intensity has the intensity multipliers across the timestamps
-# in ts. They're not normalized.
+# intensity has the intensity multipliers across the timestamps. They're not
+# normalized.
 #
 # A great idea would be a function that normalizes it and prints to a file, so
 # we can read from C to actually create the sender program.
 
-function plotHawkesSimulation(sim::HawkesSimulation)
-    plot(sim.ts, sim.intensity, xlabel = "Time", ylabel = "Intensity", label = "")
+function plotHawkesSimulation(intensity::Vector{Number})
+    plot(collect(0:0.1:T), intensity, xlabel = "Time", ylabel = "Intensity", label = "")
 end
+
+simulateAndPlot(bg::Number, kappa::Number) = plotHawkesSimulation(simulate(HawkesGenerator(bg, kappa)))
+
+intensity = simulate(HawkesGenerator(0.5, 0.5))
+
+for i in intensity
+    print(i, " ")
+end
+println()
